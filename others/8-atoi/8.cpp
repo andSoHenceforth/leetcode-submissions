b7057dcb7dbf83a964e1b5022010b4a77fec413b
Allen Darmawan <allenticular@gmail.com>
2026-03-29 10:18:56 +0200

    feat: 8.cpp

---
+++ others/8-atoi/8.cpp
@@ -0,0 +1,45 @@
+#include <limits>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+        const long long MAX_INT = INT_MAX;
+        const long long MIN_INT = INT_MIN;
+        int i = 0, n = s.length();
+
+        while (i < n && s[i] == ' ') {
+            ++i;
+        }
+
+        if (i == n) {
+            return 0;
+        }
+
+        int sign = 1;
+        if (s[i] == '+') ++i;
+        else if (s[i] == '-') {
+            sign = -1;
+            ++i;
+        }
+
+        long long res = 0;
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            res = res * 10 + digit;
+
+            if (sign * res <= INT_MIN) {
+                return INT_MIN;
+            }
+            else if (sign*res >= INT_MAX) {
+                return INT_MAX;
+            }
+            ++i;
+        }
+        return static_cast<int>(res * sign);
+    }
+};
